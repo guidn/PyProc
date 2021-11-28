4a7268b994dc546181c89056e38ef37aa22dbdf3
guidn <guioliveira852963@gmail.com>
2021-11-28 16:03:10 -0300

    JustTesting

---
+++ account.py
@@ -0,0 +1,23 @@
+#Uns importo loko ai
+
+
+
+print("Aqui está sua conta bancaria")
+
+def __name__(name):
+    name = input(print("Digite seu nome para continuar com o atendimento."))
+    
+
+
+def Abrir(abrir):
+    print("Olá, bem vindo ao seu aplicativo do banco")
+    
+    j = input(print("Voce já tem uma conta?? Digite 1 para entrar na sua conta e 2 para Criar uma nova conta."))
+    
+    if j == 1:
+        print("Entrando na sua conta")
+    else:
+        print("Criando sua nova Conta")
+        
+Abrir()
+
