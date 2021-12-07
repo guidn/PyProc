e18bcfe099e7c1c361c1ac0713e537ca74d6b368
guidn <guioliveira852963@gmail.com>
2021-12-07 14:58:50 -0300

    cansei

--- account.py
+++ account.py
@@ -1,27 +1,3 @@
 #Uns importo loko ai
 
 
-
-print("Aqui está sua conta bancaria")
-
-def __name__():
-    name = input(print("Digite seu nome para continuar com o atendimento."))
-    if name == name:
-       name = print("Olá {}, Bem vindo ao aplicativo do seu banco".format(name))
-        
-__name__()
-    
-
-
-def Abrir(abrir):
-    print("Olá {}, bem vindo ao seu aplicativo do banco".format(__name__))
-    
-    j = input(print("Voce já tem uma conta?? Digite 1 para entrar na sua conta e 2 para Criar uma nova conta."))
-    
-    if j == 1:
-        print("Entrando na sua conta")
-    else:
-        print("Criando sua nova Conta")
-        
-Abrir(Abrir)
-
---
+++ school.py
@@ -0,0 +1,21 @@
+##criando somador de media utilizando poo
+
+class media:
+    def __init__ (self, num1, num2, result, name):
+        self.num1 = num1
+        self.num2 = num2
+        self.result = result
+        self.name = name
+        
+        
+    def inicio (self):
+        i = print('OLá, vamos somar sua média, mas antes nos diga'
+                  f'Qual o seu nome {self.name}')
+        
+        
+        
+        
+        
+
+print ('Seja bem vindo a calculadora de Media, feita em Python')
+
